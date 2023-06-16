24ca115476b2b7fd15e5296913f7a90a485da400
aslak <aslakrb@gmail.com>
2023-06-16 02:36:12 +0200

    refactor

--- functions.ts
+++ functions.ts
@@ -1,5 +1,12 @@
 import type { FinnAd } from "./types.ts";
 
+const adPrefix = "https://www.finn.no/bap/forsale/ad.html?finnkode="
+
+const adToMsg = (ad: FinnAd) => {
+  return "**" + ad.heading + "** i " + ad.location + ": *" + ad.price.amount +
+    "* \n" + adPrefix + ad.id;
+};
+
 export const postToWebhook = async (ad: FinnAd, webhookUrl: string) => {
   const content = { content: adToMsg(ad) };
   const resp = await fetch(webhookUrl, {
@@ -14,7 +21,3 @@ export const postToWebhook = async (ad: FinnAd, webhookUrl: string) => {
   return resp;
 };
 
-const adToMsg = (ad: FinnAd) => {
-  return "**" + ad.heading + "** i " + ad.location + ": *" + ad.price.amount +
-    "* \n" + "https://www.finn.no/bap/forsale/ad.html?finnkode=" + ad.id;
-};
